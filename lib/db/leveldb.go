6d96942f5258d048c6270162c2bca52dcd89e37e
Jakob Borg <jakob@nym.se>
2016-06-08 22:19:01 +0200

    import proto definition where needed

--- lib/db/leveldb.go
+++ lib/db/leveldb.go
@@ -4,9 +4,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at http://mozilla.org/MPL/2.0/.
 
-//go:generate -command genxdr go run ../../vendor/github.com/calmh/xdr/cmd/genxdr/main.go
-//go:generate genxdr -o leveldb_xdr.go leveldb.go
-
 package db
 
 import (
